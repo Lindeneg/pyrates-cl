97514bc25879fc8b833fff3f26aae1abfb588b6d
wh473v3rw0rk5 <christianlindeneg@gmail.com>
2020-03-16 21:22:50 +0100

    adding cli script

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ setuptools.setup(
     url="https://github.com/lindeneg/pyrates-cl",
     packages=setuptools.find_packages(),
     install_requires=require,
+    test_suite="tests",
+    scripts=["bin/pyrates"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
