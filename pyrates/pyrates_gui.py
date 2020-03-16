75effc35b254e5489d1cdb186edea7f073112065
wh473v3rw0rk5 <christianlindeneg@gmail.com>
2020-03-16 05:28:11 +0100

    Update pyrates_gui.py

---
+++ pyrates/pyrates_gui.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, Canvas, Frame 
+from typing import Sequence, Mapping
+
+from pyrates.pyrates import PyRates
+from pyrates.util.constants import Constants, Types
+
+
+class PyRatesGUI:
+    pass
